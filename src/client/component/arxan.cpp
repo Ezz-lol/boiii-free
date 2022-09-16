f07f665ca04f6913d0593a76bc464932ef8f05b0
momo5502 <mauriceheumann@gmail.com>
2022-09-16 17:43:56 +0200

    Fix build

--- src/client/component/arxan.cpp
+++ src/client/component/arxan.cpp
@@ -426,7 +426,7 @@ namespace arxan
 		uint32_t adjust_integrity_checksum(const uint64_t return_address, uint8_t* stack_frame,
 		                                   const uint32_t current_checksum)
 		{
-			const auto handler_address = return_address - 5;
+			//const auto handler_address = return_address - 5;
 			const auto* context = search_handler_context(stack_frame, current_checksum);
 
 			if (!context)
