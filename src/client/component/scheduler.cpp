a8ae9ee86d807711f4aee725b05f90ea019a47c5
m <mjkzyalt@gmail.com>
2022-09-17 08:11:44 -0500

    add server and main pipelines

--- src/client/component/scheduler.cpp
+++ src/client/component/scheduler.cpp
@@ -165,7 +165,9 @@ namespace scheduler
 
 		void post_unpack() override
 		{
-			r_end_frame_hook.create(0x142273560_g, r_end_frame_stub);
+			r_end_frame_hook.create(0x142273560_g, r_end_frame_stub);	// some func called before R_EndFrame, maybe SND_EndFrame?
+			g_run_frame_hook.create(0x14065C360_g, server_frame_stub);	// GlassSv_Update
+			main_frame_hook.create(0x1420F9860_g, main_frame_stub);		// Com_Frame_Try_Block_Function
 		}
 
 		void pre_destroy() override
