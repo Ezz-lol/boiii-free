00e99b66132b99a05466a6ade82ed5a12e1e658b
momo5502 <mauriceheumann@gmail.com>
2022-09-17 09:16:24 +0200

    Improve branding

--- src/client/component/branding.cpp
+++ src/client/component/branding.cpp
@@ -3,6 +3,7 @@
 #include "game/game.hpp"
 
 #include <utils/hook.hpp>
+#include <version.hpp>
 
 namespace branding
 {
@@ -13,17 +14,17 @@ namespace branding
 		void draw_branding()
 		{
 			constexpr auto x = 4;
-			constexpr auto y = -5;
-			constexpr auto scale = 1.0f;
+			constexpr auto y = 0;
+			constexpr auto scale = 0.5f;
 			//float color[4] = {0.666f, 0.666f, 0.666f, 0.666f};
 			float color[4] = {236 / 255.0f, 113 / 255.0f, 10 / 255.0f, 1.0f};
 
 			auto* font = reinterpret_cast<uint32_t*(*)()>(0x141CAC8E0_g)();
 			if (!font) return;
 
-			game::R_AddCmdDrawText("BOIII", 0x7FFFFFFF, font, static_cast<float>(x),
+			game::R_AddCmdDrawText("BOIII: " VERSION, 0x7FFFFFFF, font, static_cast<float>(x),
 			                       y + static_cast<float>(font[2]) * scale,
-			                       scale, scale, 0.0f, color, 0);
+			                       scale, scale, 0.0f, color, game::ITEM_TEXTSTYLE_SHADOWEDMORE);
 		}
 
 		void r_end_frame_stub()
