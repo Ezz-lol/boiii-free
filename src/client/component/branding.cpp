8d79343cc031286fe85952ae2fe57886ca3aefa3
momo5502 <mauriceheumann@gmail.com>
2022-09-18 08:33:11 +0200

    Better branding

--- src/client/component/branding.cpp
+++ src/client/component/branding.cpp
@@ -15,15 +15,15 @@ namespace branding
 		{
 			constexpr auto x = 4;
 			constexpr auto y = 0;
-			constexpr auto scale = 0.5f;
-			float color[4] = {236 / 255.0f, 113 / 255.0f, 10 / 255.0f, 1.0f};
+			constexpr auto scale = 0.45f;
+			float color[4] = {0.666f, 0.666f, 0.666f, 0.666f};
 
 			auto* font = reinterpret_cast<uint32_t*(*)()>(0x141CAC8E0_g)();
 			if (!font) return;
 
 			game::R_AddCmdDrawText("BOIII: " VERSION, 0x7FFFFFFF, font, static_cast<float>(x),
 			                       y + static_cast<float>(font[2]) * scale,
-			                       scale, scale, 0.0f, color, game::ITEM_TEXTSTYLE_SHADOWEDMORE);
+			                       scale, scale, 0.0f, color, game::ITEM_TEXTSTYLE_NORMAL);
 		}
 	}
 
