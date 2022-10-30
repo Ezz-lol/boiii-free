b268f0c5e0c906fa305708bb0f7dea6560d4593f
momo5502 <mauriceheumann@gmail.com>
2022-10-30 11:43:06 +0100

    Change application icon

---
+++ src/client/component/icon.cpp
@@ -0,0 +1,42 @@
+#include <std_include.hpp>
+#include "loader/component_loader.hpp"
+#include "resource.hpp"
+
+#include <utils/nt.hpp>
+#include <utils/hook.hpp>
+
+namespace icon
+{
+	namespace
+	{
+		utils::hook::detour load_icon_a_hook;
+
+		HINSTANCE get_current_module()
+		{
+			return utils::nt::library::get_by_address(get_current_module);
+		}
+
+		HICON WINAPI load_icon_a_stub(HINSTANCE module, LPCSTR icon_name)
+		{
+			static const utils::nt::library game{};
+			if (game == module && icon_name == MAKEINTRESOURCEA(1))
+			{
+				module = get_current_module();
+				icon_name = MAKEINTRESOURCEA(ID_ICON);
+			}
+
+			return load_icon_a_hook.invoke<HICON>(module, icon_name);
+		}
+	}
+
+	class component final : public component_interface
+	{
+	public:
+		component()
+		{
+			load_icon_a_hook.create(LoadIconA, load_icon_a_stub);
+		}
+	};
+}
+
+REGISTER_COMPONENT(icon::component)
