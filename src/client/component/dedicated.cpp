b1aa9ac4aa7b184995d8e34d22b72cc28b7250f3
momo5502 <mauriceheumann@gmail.com>
2022-11-24 17:25:37 +0100

    Prepare dedicated component

---
+++ src/client/component/dedicated.cpp
@@ -0,0 +1,23 @@
+#include <std_include.hpp>
+#include "loader/component_loader.hpp"
+
+#include <utils/hook.hpp>
+
+namespace dedicated
+{
+	namespace
+	{
+		
+	}
+
+	class component final : public component_interface
+	{
+	public:
+		void post_unpack() override
+		{
+			
+		}
+	};
+}
+
+REGISTER_COMPONENT(dedicated::component)
