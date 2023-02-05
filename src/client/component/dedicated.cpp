541cbba1e2677f7409389faae99a9fe4f8afc9cf
Maurice Heumann <mauriceheumann@gmail.com>
2023-02-05 19:39:44 +0100

    Regularly send server heatbeats

--- src/client/component/dedicated.cpp
+++ src/client/component/dedicated.cpp
@@ -2,6 +2,10 @@
 #include "loader/component_loader.hpp"
 
 #include "game/game.hpp"
+#include "command.hpp"
+#include "network.hpp"
+#include "scheduler.hpp"
+#include "server_list.hpp"
 
 #include <utils/hook.hpp>
 
@@ -12,6 +16,15 @@ namespace dedicated
 		void sv_con_tell_f_stub(game::client_s* cl_0, game::svscmd_type type, [[maybe_unused]] const char* fmt, [[maybe_unused]] int c, char* text)
 		{
 			game::SV_SendServerCommand(cl_0, type, "%c \"GAME_SERVER\x15: %s\"", 79, text);
+		}
+
+		void send_heartbeat()
+		{
+			game::netadr_t target{};
+			if (server_list::get_master_server(target))
+			{
+				network::send(target, "heartbeat", "T7");
+			}
 		}
 	}
 
@@ -25,6 +38,10 @@ namespace dedicated
 
 			// Fix tell command for IW4M
 			utils::hook::call(0x14052A8CF_g, sv_con_tell_f_stub);
+
+			scheduler::once(send_heartbeat, scheduler::pipeline::server);
+			scheduler::loop(send_heartbeat, scheduler::pipeline::server, 10min);
+			command::add("heartbeat", send_heartbeat);
 		}
 	};
 }
