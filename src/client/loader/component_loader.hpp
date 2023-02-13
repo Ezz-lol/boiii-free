d8c561d26e4092c1e2d5dc1e2776d1ba54a21c55
Maurice Heumann <mauriceheumann@gmail.com>
2023-02-13 18:47:04 +0100

    Remove redundant static

--- src/client/loader/component_loader.hpp
+++ src/client/loader/component_loader.hpp
@@ -38,8 +38,8 @@ namespace component_loader
 	};
 };
 
-#define REGISTER_COMPONENT(name)                          \
-namespace                                                 \
-{                                                         \
-	static component_loader::installer<name> __component; \
+#define REGISTER_COMPONENT(name)                   \
+namespace                                          \
+{                                                  \
+	component_loader::installer<name> __component; \
 }
