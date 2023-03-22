4668f8afb1313431ea60c7a00f09a664eb9f75bb
Edo <edoardo.sanguineti222@gmail.com>
2023-03-22 13:36:44 +0000

    maint(utils): use * to indicate pointers

--- src/client/game/utils.cpp
+++ src/client/game/utils.cpp
@@ -7,7 +7,7 @@ namespace game
 {
 	std::string get_dvar_string(const char* dvar_name)
 	{
-		const auto dvar = Dvar_FindVar(dvar_name);
+		const auto* dvar = Dvar_FindVar(dvar_name);
 		if (!dvar)
 		{
 			return {};
@@ -18,7 +18,7 @@ namespace game
 
 	int get_dvar_int(const char* dvar_name)
 	{
-		const auto dvar = Dvar_FindVar(dvar_name);
+		const auto* dvar = Dvar_FindVar(dvar_name);
 		if (!dvar)
 		{
 			return {};
@@ -29,7 +29,7 @@ namespace game
 
 	bool get_dvar_bool(const char* dvar_name)
 	{
-		const auto dvar = Dvar_FindVar(dvar_name);
+		const auto* dvar = Dvar_FindVar(dvar_name);
 		if (!dvar)
 		{
 			return {};
@@ -41,7 +41,7 @@ namespace game
 	const dvar_t* register_sessionmode_dvar_bool(const char* dvar_name, const bool value, const int flags, const char* description, const eModes mode)
 	{
 		const auto hash = Dvar_GenerateHash(dvar_name);
-		auto registered_dvar = Dvar_SessionModeRegisterBool(hash, dvar_name, value, flags, description);
+		auto* registered_dvar = Dvar_SessionModeRegisterBool(hash, dvar_name, value, flags, description);
 
 		if (registered_dvar)
 		{
@@ -66,7 +66,7 @@ namespace game
 	const dvar_t* register_dvar_bool(const char* dvar_name, const bool value, const int flags, const char* description)
 	{
 		const auto hash = Dvar_GenerateHash(dvar_name);
-		auto registered_dvar = Dvar_RegisterBool(hash, dvar_name, value, flags, description);
+		auto* registered_dvar = Dvar_RegisterBool(hash, dvar_name, value, flags, description);
 
 		if (registered_dvar)
 		{
@@ -79,7 +79,7 @@ namespace game
 	const dvar_t* register_dvar_string(const char* dvar_name, const char* value, const int flags, const char* description)
 	{
 		const auto hash = Dvar_GenerateHash(dvar_name);
-		auto registered_dvar = Dvar_RegisterString(hash, dvar_name, value, flags, description);
+		auto* registered_dvar = Dvar_RegisterString(hash, dvar_name, value, flags, description);
 
 		if (registered_dvar)
 		{
@@ -91,14 +91,14 @@ namespace game
 
 	void dvar_add_flags(const char* dvar_name, const dvarFlags_e flags)
 	{
-		auto dvar = Dvar_FindVar(dvar_name);
+		auto* dvar = Dvar_FindVar(dvar_name);
 
 		if (!dvar)
 		{
 			return;
 		}
 
-		auto dvar_to_change = dvar;
+		auto* dvar_to_change = dvar;
 
 		if (dvar_to_change->type == DVAR_TYPE_SESSIONMODE_BASE_DVAR)
 		{
@@ -111,14 +111,14 @@ namespace game
 
 	void dvar_set_flags(const char* dvar_name, const dvarFlags_e flags)
 	{
-		auto dvar = Dvar_FindVar(dvar_name);
+		auto* dvar = Dvar_FindVar(dvar_name);
 
 		if (!dvar)
 		{
 			return;
 		}
 
-		auto dvar_to_change = dvar;
+		auto* dvar_to_change = dvar;
 
 		if (dvar_to_change->type == DVAR_TYPE_SESSIONMODE_BASE_DVAR)
 		{
