6ac281c259db88b23598f97f193a233b80880047
momo5502 <mauriceheumann@gmail.com>
2023-04-02 19:46:02 +0200

    Small cleanup

--- src/common/utils/info_string.cpp
+++ src/common/utils/info_string.cpp
@@ -42,25 +42,25 @@ namespace utils
 			buffer = buffer.substr(1);
 		}
 
-		auto key_values = string::split(buffer, '\\');
+		const auto key_values = string::split(buffer, '\\');
 		for (size_t i = 0; !key_values.empty() && i < (key_values.size() - 1); i += 2)
 		{
 			const auto& key = key_values[i];
 			const auto& value = key_values[i + 1];
-			this->key_value_pairs_[key] = value;
+
+			if (!this->key_value_pairs_.contains(key))
+			{
+				this->key_value_pairs_[key] = value;
+			}
 		}
 	}
 
 	std::string info_string::build() const
 	{
-		//auto first = true;
 		std::string info_string;
 		for (auto i = this->key_value_pairs_.begin(); i != this->key_value_pairs_.end(); ++i)
 		{
-			//if (first) first = false;
-			/*else*/
 			info_string.append("\\");
-
 			info_string.append(i->first); // Key
 			info_string.append("\\");
 			info_string.append(i->second); // Value
