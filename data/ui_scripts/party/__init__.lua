03a09ad4c372db1762213fabf7331c070f3c2fc4
WantedDV <122710241+WantedDV@users.noreply.github.com>
2023-04-11 17:26:50 -0230

    set com_maxclients ingame to allow direct connect
    - 4 for zombies
    - 18 for multiplayer

--- data/ui_scripts/party/__init__.lua
+++ data/ui_scripts/party/__init__.lua
@@ -1,4 +1,3 @@
-
 -- Removed check for public matches to allow team change in ranked matches
 CoD.IsTeamChangeAllowed = function()
 	if Engine.GetGametypeSetting( "allowInGameTeamChange" ) == 1 then
@@ -7,6 +6,18 @@ CoD.IsTeamChangeAllowed = function()
 		return false
 	end
 end
+
+-- Set com_maxclients InGame so players can join via direct connect
+local maxclients = function()
+	if Engine.IsZombiesGame() then
+		return 4
+	else
+		return 18
+	end
+end
+
+Engine.SetDvar( "com_maxclients", maxclients() )
+
 
 DataSources.StartMenuGameOptions = ListHelper_SetupDataSource("StartMenuGameOptions", function (controller)
 	local options = {}
