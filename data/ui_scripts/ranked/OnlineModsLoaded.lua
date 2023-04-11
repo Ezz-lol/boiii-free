ceb546b448c260b595169d1d43ec029d6148d494
WantedDV <122710241+WantedDV@users.noreply.github.com>
2023-04-11 17:34:19 -0230

    Auto-load usermaps mod if required for custom map

--- data/ui_scripts/ranked/OnlineModsLoaded.lua
+++ data/ui_scripts/ranked/OnlineModsLoaded.lua
@@ -6,3 +6,20 @@ function ShouldUnloadMod( arg0 )
 	return false
 end
 
+-- Load usermaps mod if custom map selected
+local MustLoadUsermaps = function( arg0, arg1 )  
+	if CoD.perController[arg1].mapCategory == CoD.CONTENT_MODS_INDEX then
+		if not Engine.IsUsingMods() then
+			LuaUtils.Mods_LoadMod( "usermaps" ) -- LuaUtils.Mods_LoadMod(arg0.loadModName)
+	
+			local MENU_MODS_CAPS = Engine.Localize( "MENU_MODS_CAPS" )
+			local PLATFORM_MODS_LOADED = Engine.Localize( "PLATFORM_MODS_LOADED" )
+			CoD.OverlayUtility.ShowToast( "DefaultState", MENU_MODS_CAPS, PLATFORM_MODS_LOADED .. " " ..  arg0.mapName )
+		end
+	end
+end
+
+function MapSelected( arg0, arg1 )
+	SetMap( arg1, arg0.mapName, false )
+	MustLoadUsermaps( arg0, arg1 )
+end
