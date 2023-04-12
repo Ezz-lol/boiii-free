985610739b7953aadd8b2e458cc57ff620dd90c3
WantedDV <122710241+WantedDV@users.noreply.github.com>
2023-04-12 20:01:46 -0230

    Lua formatting

--- data/ui_scripts/ranked/online_mods_loaded.lua
+++ data/ui_scripts/ranked/online_mods_loaded.lua
@@ -2,24 +2,24 @@ if Engine.GetCurrentMap() ~= "core_frontend" then
 	return
 end
 
-function ShouldUnloadMod( arg0 )
+function ShouldUnloadMod(arg0)
 	return false
 end
 
 -- Load usermaps mod if custom map selected
-local MustLoadUsermaps = function( arg0, arg1 )  
+local mustLoadUsermaps = function(arg0, arg1)
 	if CoD.perController[arg1].mapCategory == CoD.CONTENT_MODS_INDEX then
 		if not Engine.IsUsingMods() then
-			LuaUtils.Mods_LoadMod( "usermaps" ) -- LuaUtils.Mods_LoadMod(arg0.loadModName)
-	
-			local MENU_MODS_CAPS = Engine.Localize( "MENU_MODS_CAPS" )
-			local PLATFORM_MODS_LOADED = Engine.Localize( "PLATFORM_MODS_LOADED" )
-			CoD.OverlayUtility.ShowToast( "DefaultState", MENU_MODS_CAPS, PLATFORM_MODS_LOADED .. " " ..  arg0.mapName )
+			LuaUtils.Mods_LoadMod("usermaps") -- LuaUtils.Mods_LoadMod(arg0.loadModName)
+
+			local MENU_MODS_CAPS = Engine.Localize("MENU_MODS_CAPS")
+			local PLATFORM_MODS_LOADED = Engine.Localize("PLATFORM_MODS_LOADED")
+			CoD.OverlayUtility.ShowToast("DefaultState", MENU_MODS_CAPS, PLATFORM_MODS_LOADED .. " " .. arg0.mapName)
 		end
 	end
 end
 
-function MapSelected( arg0, arg1 )
-	SetMap( arg1, arg0.mapName, false )
-	MustLoadUsermaps( arg0, arg1 )
+function MapSelected(arg0, arg1)
+	SetMap(arg1, arg0.mapName, false)
+	mustLoadUsermaps(arg0, arg1)
 end
