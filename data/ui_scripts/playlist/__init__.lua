397601f25a3be07cacad5450c80f1099a3e0e1d7
WantedDV <122710241+WantedDV@users.noreply.github.com>
2023-04-15 05:29:01 -0230

    whitespace formatting

--- data/ui_scripts/playlist/__init__.lua
+++ data/ui_scripts/playlist/__init__.lua
@@ -10,12 +10,12 @@ function IsLobbyStatusVisible()
 	return false
 end
 
-Engine.SetDvar( "lobbyMigrate_Enabled", 0 )
-Engine.SetDvar( "lobbyTimerStatusVotingInterval", 11000 )
-Engine.SetDvar( "lobbyTimerStatusBeginInterval", 10 )
-Engine.SetDvar( "lobbyTimerStatusStartInterval", 10 )
-Engine.SetDvar( "lobbyTimerStatusPostGameInterval", 10 )
-Engine.SetDvar( "lobbyTimerStatusVotingInterval_Arena", 11000 )
+Engine.SetDvar("lobbyMigrate_Enabled", 0)
+Engine.SetDvar("lobbyTimerStatusVotingInterval", 11000)
+Engine.SetDvar("lobbyTimerStatusBeginInterval", 10)
+Engine.SetDvar("lobbyTimerStatusStartInterval", 10)
+Engine.SetDvar("lobbyTimerStatusPostGameInterval", 10)
+Engine.SetDvar("lobbyTimerStatusVotingInterval_Arena", 11000)
 
 require("widget_playlist_match_settings_info")
 require("widget_playlist_category_match_settings_info")
