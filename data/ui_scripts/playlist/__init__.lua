4209474b42a0f2e8a35d17552754c76f480f9ea8
WantedDV <122710241+WantedDV@users.noreply.github.com>
2023-04-17 00:45:46 -0230

    Remove searching for player text in lobby
    - No longer sets LobbyMaxClients to 1 as that disallowed splitscreen

--- data/ui_scripts/playlist/__init__.lua
+++ data/ui_scripts/playlist/__init__.lua
@@ -2,8 +2,23 @@ if Engine.GetCurrentMap() ~= "core_frontend" then
 	return
 end
 
-Engine.GetLobbyMaxClients = function()
-	return 1
+if not CoD.LobbyMember then
+	return
+end
+
+local oldLobbyMember = CoD.LobbyMember.new
+function CoD.LobbyMember.new(menu, controller)
+	local self = oldLobbyMember(menu, controller)
+
+	-- Hide the playlist count text
+	if self.SearchingForPlayer then
+		self.SearchingForPlayer:setAlpha(0)
+	end
+	if self.FEMemberBlurPanelContainer0 then
+		self.FEMemberBlurPanelContainer0:setAlpha(0)
+	end
+
+	return self
 end
 
 function IsLobbyStatusVisible()
