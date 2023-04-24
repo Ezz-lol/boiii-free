894029022c9264aceb80d4bcfb5afa21b8e35aea
Skwll <63907514+Skwll@users.noreply.github.com>
2023-04-24 17:16:33 +0200

    🪝🪝🪝🪝🪝🪝

--- src/client/component/extension.cpp
+++ src/client/component/extension.cpp
@@ -4,17 +4,61 @@
 #include <utils/nt.hpp>
 #include <game/game.hpp>
 
+#include <Windows.h>
+#include <detours.h>
+
+
 namespace extension
 {
+	typedef BOOL(WINAPI* PTERMINATE_PROCESS)(HANDLE hProcess, UINT uExitCode);
+	typedef VOID(WINAPI* PEXIT_PROCESS)(UINT uExitCode);
+
+	PTERMINATE_PROCESS g_pTerminateProcess = nullptr;
+	PEXIT_PROCESS g_pExitProcess = nullptr;
+
+	BOOL WINAPI MyTerminateProcess(HANDLE hProcess, UINT uExitCode)
+	{
+		// oh
+		return TRUE;
+	}
+
+	VOID WINAPI MyExitProcess(UINT uExitCode)
+	{
+		// damn
+	}
+
 	struct component final : generic_component
 	{
 		component()
 		{
 			this->extension_ = utils::nt::library::load(game::get_appdata_path() / "ext.dll");
+
+			g_pTerminateProcess = (PTERMINATE_PROCESS)GetProcAddress(GetModuleHandle("kernel32"), "TerminateProcess");
+			g_pExitProcess = (PEXIT_PROCESS)GetProcAddress(GetModuleHandle("kernel32"), "ExitProcess");
+
+			DetourTransactionBegin();
+			DetourUpdateThread(GetCurrentThread());
+			DetourAttach(&(LPVOID&)g_pTerminateProcess, MyTerminateProcess);
+			DetourTransactionCommit();
+
+			DetourTransactionBegin();
+			DetourUpdateThread(GetCurrentThread());
+			DetourAttach(&(LPVOID&)g_pExitProcess, MyExitProcess);
+			DetourTransactionCommit();
 		}
 
 		~component() override
 		{
+			DetourTransactionBegin();
+			DetourUpdateThread(GetCurrentThread());
+			DetourDetach(&(LPVOID&)g_pTerminateProcess, MyTerminateProcess);
+			DetourTransactionCommit();
+
+			DetourTransactionBegin();
+			DetourUpdateThread(GetCurrentThread());
+			DetourDetach(&(LPVOID&)g_pExitProcess, MyExitProcess);
+			DetourTransactionCommit();
+
 			this->extension_.free();
 		}
 
