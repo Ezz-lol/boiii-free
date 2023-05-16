113a3ac7e316befed02742a04a2f8368fef9f6f0
momo5502 <mauriceheumann@gmail.com>
2023-05-16 07:28:23 +0200

    Choose different base to prevent conflicts with non-aslr dedis

--- src/client/std_include.cpp
+++ src/client/std_include.cpp
@@ -1,5 +1,6 @@
 #include <std_include.hpp>
 
+#pragma comment(linker, "/base:0x170000000")
 #pragma comment(linker, "/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language=''\"")
 
 extern "C"
