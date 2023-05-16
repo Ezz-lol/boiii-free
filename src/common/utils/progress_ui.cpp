3fa6515eb4de2dfa29676c3fea48dd08f231915e
momo5502 <mauriceheumann@gmail.com>
2023-05-16 17:31:58 +0200

    Disable progress ui on wine

--- src/common/utils/progress_ui.cpp
+++ src/common/utils/progress_ui.cpp
@@ -4,42 +4,78 @@
 
 namespace utils
 {
-	progress_ui::progress_ui()
+	progress_ui::progress_ui(const bool allow_failure)
 	{
-		this->dialog_ = utils::com::create_progress_dialog();
-		if (!this->dialog_)
+		try
 		{
-			throw std::runtime_error{"Failed to create dialog"};
+			if(utils::nt::is_wine())
+			{
+				throw std::runtime_error{ "Disabled on wine" };
+			}
+
+			this->dialog_ = utils::com::create_progress_dialog();
+			if (!this->dialog_)
+			{
+				throw std::runtime_error{"Failed to create dialog"};
+			}
+		}
+		catch (...)
+		{
+			if (!allow_failure)
+			{
+				throw;
+			}
 		}
 	}
 
 	progress_ui::~progress_ui()
 	{
-		this->dialog_->StopProgressDialog();
+		if (this->dialog_)
+		{
+			this->dialog_->StopProgressDialog();
+		}
 	}
 
 	void progress_ui::show(const bool marquee, HWND parent) const
 	{
-		this->dialog_->StartProgressDialog(parent, nullptr, PROGDLG_AUTOTIME | (marquee ? PROGDLG_MARQUEEPROGRESS : 0), nullptr);
+		if (this->dialog_)
+		{
+			this->dialog_->StartProgressDialog(parent, nullptr,
+			                                   PROGDLG_AUTOTIME | (marquee ? PROGDLG_MARQUEEPROGRESS : 0), nullptr);
+		}
 	}
 
 	void progress_ui::set_progress(const size_t current, const size_t max) const
 	{
-		this->dialog_->SetProgress64(current, max);
+		if (this->dialog_)
+		{
+			this->dialog_->SetProgress64(current, max);
+		}
 	}
 
 	void progress_ui::set_line(const int line, const std::string& text) const
 	{
-		this->dialog_->SetLine(line, utils::string::convert(text).data(), false, nullptr);
+		if (this->dialog_)
+		{
+			this->dialog_->SetLine(line, utils::string::convert(text).data(), false, nullptr);
+		}
 	}
 
 	void progress_ui::set_title(const std::string& title) const
 	{
-		this->dialog_->SetTitle(utils::string::convert(title).data());
+		if (this->dialog_)
+		{
+			this->dialog_->SetTitle(utils::string::convert(title).data());
+		}
 	}
 
 	bool progress_ui::is_cancelled() const
 	{
-		return this->dialog_->HasUserCancelled();
+		if (this->dialog_)
+		{
+			return this->dialog_->HasUserCancelled();
+		}
+
+		return false;
 	}
 }
