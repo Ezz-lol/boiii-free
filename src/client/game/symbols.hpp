ea15df4e5420e59bee259ae2c7890326b8a30284
Adam Killam <adammkillam@gmail.com>
2026-04-14 20:53:43 -0230

    Prefer more self-documenting type for g_zoneNameList

--- src/client/game/symbols.hpp
+++ src/client/game/symbols.hpp
@@ -111,7 +111,7 @@ WEAK symbol<uint32_t> g_zoneCount{0x14941097C, 0x14699D21C};
 WEAK symbol<uint32_t> g_zoneIndex{0x0, 0x1469BB268};
 WEAK symbol<ZonePool<XZoneName>> g_zoneNames{0x14998FB80, 0x146E83270};
 WEAK symbol<ZonePool<XZone>> g_zones{0x0, 0x146E84AD0};
-WEAK symbol<char[4160]> g_zoneNameList{0x0, 0x146E876D0};
+WEAK symbol<ZonePool<char[65]>> g_zoneNameList{0x0, 0x146E876D0};
 WEAK symbol<ZonePool<XZoneInfoInternal>> g_zoneInfo{0x149995020, 0x146E88730};
 WEAK symbol<uint32_t> g_zoneInfoCount{0x149996620, 0x146E88714};
 
