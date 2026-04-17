4ffa4f46922e2595cc5377487a87d171fbdb843c
Adam Killam <adammkillam@gmail.com>
2026-04-17 08:15:52 -0230

    Add DB_GetAssetEntryPoolEntryByName` to include

--- src/client/game/db/db.hpp
+++ src/client/game/db/db.hpp
@@ -10,6 +10,8 @@ void reallocate_asset_pool(const XAssetType type, const unsigned int new_size);
 
 // Always inlined in engine, reimplemented here for use
 uint32_t DB_HashForName(const char *name, const XAssetType type);
+XAssetEntryPoolEntry *DB_GetAssetEntryPoolEntryByName(const char *name,
+                                                      const XAssetType type);
 
 } // namespace xasset
 } // namespace db
