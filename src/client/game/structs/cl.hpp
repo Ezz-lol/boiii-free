4bdf8a88cba0deb5379de794614c066a6b3ec95b
Adam Killam <adammkillam@gmail.com>
2026-05-14 21:22:49 -0230

    Define majority of CL data structures, types related to server connection, initial map load, server disconnect

--- src/client/game/structs/cl.hpp
+++ src/client/game/structs/cl.hpp
@@ -2,6 +2,8 @@
 
 #include <cstdint>
 #include "core.hpp"
+#include "net/net.hpp"
+#include "phys.hpp"
 
 namespace game {
 namespace cl {
@@ -9,9 +11,10 @@ namespace cl {
 enum class CLSwitchState : uint32_t {
   CL_SWITCH_STATE_IDLE = 0x0,
   CL_SWITCH_WAIT_FOR_CLIENTS_TO_CONNECT = 0x1,
-  CL_SWITCH_STATE_LOAD_FASTFILE = 0x2,
-  CL_SWITCH_STATE_PRIME_STREAMER = 0x3,
-  CL_SWITCH_STATE_SWITCH_MAPS = 0x4,
+  CL_SWITCH_STATE_PRELOAD_RESERVE = 0x2,
+  CL_SWITCH_STATE_LOAD_FASTFILE = 0x3,
+  CL_SWITCH_STATE_PRIME_STREAMER = 0x4,
+  CL_SWITCH_STATE_SWITCH_MAPS = 0x5,
 };
 
 enum CLSwitchMsg : uint32_t {
@@ -52,14 +55,172 @@ struct CLMapSwitch {
   uint32_t preloadReservePagesNeeded;
   cinematic_id loadingMovieId;
   CLSwitchMessage messages[16];
-  int messageCount;
+  int32_t messageCount;
   str64_t strArgs[16];
-  int strCount;
-  int timeFastfileLoadStarted;
-  int timeTextureStreamingStarted;
+  int32_t strCount;
+  int32_t timeFastfileLoadStarted;
+  int32_t timeTextureStreamingStarted;
 };
 static_assert(sizeof(CLMapSwitch) == 0x7B0,
               "CLMapSwitch size must be 0x7B0 bytes");
 #pragma pack(pop)
+
+#pragma pack(push, 1)
+struct ReliableCommands {
+  int32_t sequence;
+  int32_t acknowledge;
+  char *commands[128];
+  int32_t commandBufferNext;
+  char commandBuffer[16384];
+  uint8_t _padding[4];
+};
+
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+struct clientConnection_t {
+  int32_t qport;
+  ClientNum_t clientNum;
+  int32_t lastPacketSentTime;
+  int32_t lastPacketTime;
+  net::netadr_t serverAddress;
+  int32_t connectTime;
+  int32_t connectPacketCount;
+  char serverMessage[256];
+  int32_t challenge;
+  int32_t checksumFeed;
+  ReliableCommands reliableCommands;
+  int32_t serverMessageSequence;
+  int32_t serverCommandSequence;
+  int32_t lastExecutedServerCommand;
+  char serverCommands[128][1024];
+  bool isServerRestarting;
+  bool areTexturesLoaded;
+  bool waitForMovie;
+  bool hostCompromised;
+  net::netchan_t netchan;
+  uint64_t serverXUID;
+  net::netProfileInfo_t OOBProf;
+  uint8_t _unknown25150[1032];
+  uint8_t *transferBuffer;
+  int32_t transferBufferCompressedSize;
+  uint8_t _padding25564[4];
+  int64_t statPacketsToSend[2];
+  int32_t statPacketSendTime[127];
+  int32_t loadoutNonceSent;
+  int32_t loadoutNonceReceived;
+  int32_t loadoutNextRetry;
+};
+static_assert(sizeof(clientConnection_t) == 0x25780,
+              "sizeof(clientConnection_t) != 0x25780");
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+// Unverified
+struct clientLogo_t {
+  int32_t startTime;
+  int32_t duration;
+  int32_t fadein;
+  int32_t fadeout;
+  phys::MaterialHandle material[2];
+};
+static_assert(sizeof(clientLogo_t) == 0x20, "sizeof(clientLogo_t) != 0x20");
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+// Unverified
+struct vidConfig_t {
+  uint32_t sceneWidth;
+  uint32_t sceneHeight;
+  float sceneAspectRatio;
+  uint32_t displayWidth;
+  uint32_t displayHeight;
+  float displayAspectRatio;
+  qboolean isWideScreen;
+  qboolean isFullscreen;
+  float aspectRatioWindow;
+  float aspectRatioUI;
+  float aspectRatioDisplayPixel;
+  uint32_t maxTextureSize;
+  qboolean deviceSupportsGamma;
+};
+static_assert(sizeof(vidConfig_t) == 0x34, "sizeof(vidConfig_t) != 0x34");
+#pragma pack(pop)
+
+// TODO when playerState_t defined and verified
+// #pragma pack(pop)
+// #pragma pack(push, 1)
+// struct clSnapshot_t {
+//   qboolean valid;
+//   int32_t snapFlags;
+//   int32_t serverTime;
+//   int32_t physicsTime;
+//   int32_t messageNum;
+//   int32_t deltaNum;
+//   int32_t ping;
+//   int32_t cmdNum;
+//   level::playerState_t ps;
+//   int32_t numEntities;
+//   int32_t numClients;
+//   int32_t numActors;
+//   int32_t parseMatchStateIndex;
+//   int32_t parseEntitiesIndex;
+//   int32_t parseClientsIndex;
+//   int32_t parseActorsIndex;
+//   int32_t serverCommandNum;
+// };
+// #pragma pack(pop)
+
+#pragma pack(push, 1)
+struct clientStatic_t {
+  qboolean hunkUsersStarted;
+  char servername[256];
+  qboolean rendererStarted;
+  qboolean soundStarted;
+  qboolean uiStarted;
+  int32_t frametime;
+  float frametime_base;
+  int32_t _unknown118;
+  int32_t realtime;
+  int32_t realFrametime; // only verified to here
+  // TODO when following fields, underlying structs
+  // defined and verified
+
+  // qboolean inputUpdatedPrevFrame;
+  // clientLogo_t logo;
+  // vec3_t mapCenter;
+  // int32_t numlocalservers;
+  // net::serverInfo_t localServers[64];
+  // int32_t pingUpdateSource;
+  // uint8_t _padding3B5C[4];
+  // phys::MaterialHandle whiteMaterial;
+  // phys::MaterialHandle consoleMaterial;
+  // phys::MaterialHandle spinnerMaterial;
+  // phys::MaterialHandle loaderMaterial;
+  // phys::MaterialHandle cinematicMaterial;
+  // FontHandle consoleFont;
+  // cinematic_id logoMovieId;
+  // qboolean playedLogoCinematic;
+  // vidConfig_t vidConfig;
+  // vec3_t renderForward;
+  // vec3_t renderPos;
+  // net::XNADDR xnaddrs[18];
+  // uint8_t _padding3E7E[2];
+  // volatile int32_t scriptError;
+  // vec3_t debugRenderForward;
+  // vec3_t debugRenderPos;
+  // int32_t serverId;
+  // qboolean forceFullscreen;
+  // qboolean exitForceFullscreen;
+  // uint8_t _padding3EA8[8];
+  // clSnapshot_t noDeltaSnapshot;
+  // int32_t nextNoDeltaEntity;
+  // uint8_t _paddingF414[4];
+  // level::entityState_t noDeltaEntities[1024];
+  // level::gameState_t gameState;
+};
+
+#pragma pack(pop)
+
 } // namespace cl
 } // namespace game
