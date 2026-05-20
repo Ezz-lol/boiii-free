a4fdd941c1f7fe7191cdf6104e28911614ad45dd
Adam Killam <adammkillam@gmail.com>
2026-05-20 00:58:12 -0230

    Small cleanup

--- src/client/component/script.cpp
+++ src/client/component/script.cpp
@@ -557,8 +557,9 @@ void load_scripts() {
   std::vector<std::filesystem::path> applicable_custom_script_paths = {
       "custom_scripts/shared", "custom_scripts/core",
       "custom_scripts/codescripts"};
-  if (const auto game_type = get_game_type_specific_folder();
-      game_type.has_value()) {
+  const std::optional<std::filesystem::path> game_type =
+      get_game_type_specific_folder();
+  if (game_type.has_value()) {
     applicable_custom_script_paths.push_back("custom_scripts" /
                                              game_type.value());
   }
