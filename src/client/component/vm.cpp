6bba537224cd0ebdd451756a6e327633b1897c0c
Adam Killam <adammkillam@gmail.com>
2026-07-23 21:33:05 -0230

    Include missing header for hooks

--- src/client/component/vm.cpp
+++ src/client/component/vm.cpp
@@ -3,21 +3,24 @@
 
 #include <game/game.hpp>
 #include <game/utils.hpp>
+#include <utils/hook.hpp>
 
 namespace vm {
 inline bool vm_op_call_state_valid(game::scr::scriptInstance_t inst) {
-  const auto *frame = game::scr::vm::gScrVmPub->instance[inst].function_frame;
+  const game::scr::vm::function_frame_t *frame =
+      game::scr::vm::gScrVmPub->instance[inst].function_frame;
   return game::valid_module_ptr(frame) &&
-        game::valid_module_ptr(frame->fs.startTop) &&
-        game::valid_module_ptr(frame->fs.top);
+         game::valid_module_ptr(frame->fs.startTop) &&
+         game::valid_module_ptr(frame->fs.top);
 }
 
 void log_invalid_vm_state(const char *handler_name) {
-  fprintf(stderr, "[vm] skipped %s: invalid function_frame state\n",
-         handler_name);
-  game::com::Com_Printf(
-      0, game::consoleLabel_e::DEFAULT,
-      "[vm] skipped %s: invalid function_frame state\n", handler_name);
+  fprintf(stderr, "[ScrVM] skipped %s: invalid function_frame state\n",
+          handler_name);
+  fflush(stderr);
+  game::com::Com_Printf(0, game::consoleLabel_e::DEFAULT,
+                        "[ScrVM] skipped %s: invalid function_frame state\n",
+                        handler_name);
 }
 
 utils::hook::detour VM_OP_ScriptFunctionCall_Handler_hook;
