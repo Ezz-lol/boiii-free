18fff5d12aab81a53e3d3930f88d0bbefe689572
Adam Killam <adammkillam@gmail.com>
2026-08-30 11:12:48 -0230

    Add missing `Axios` lua library functions used in T7Recharged

--- src/client/component/axios.cpp
+++ src/client/component/axios.cpp
@@ -13,12 +13,17 @@ using namespace game::lua;
 luaReturnCount_e get(lua_State *s) {
   const char *url = lua_tostring(s, 1);
 
-  http::Request request{url};
-  const http::Response response = request.send("GET");
+  try {
+    http::Request request{url};
 
-  const std::string responseBody =
-      std::string{response.body.begin(), response.body.end()};
-  lua_pushstring(s, responseBody.c_str());
+    const http::Response response = request.send("GET");
+
+    const std::string responseBody =
+        std::string{response.body.begin(), response.body.end()};
+    lua_pushstring(s, responseBody.c_str());
+  } catch (...) {
+    lua_pushstring(s, "");
+  }
 
   return luaReturnCount_e::ONE;
 }
@@ -27,13 +32,17 @@ luaReturnCount_e post(lua_State *s) {
   const char *url = lua_tostring(s, 1);
   const char *body = lua_tostring(s, 2);
 
-  http::Request request{url};
-  const http::Response response =
-      request.send("POST", body, {{"Content-Type", "application/json"}});
+  try {
+    http::Request request{url};
+    const http::Response response =
+        request.send("POST", body, {{"Content-Type", "application/json"}});
 
-  const std::string responseBody =
-      std::string{response.body.begin(), response.body.end()};
-  lua_pushstring(s, responseBody.c_str());
+    const std::string responseBody =
+        std::string{response.body.begin(), response.body.end()};
+    lua_pushstring(s, responseBody.c_str());
+  } catch (...) {
+    lua_pushstring(s, "");
+  }
 
   return luaReturnCount_e::ONE;
 }
@@ -41,9 +50,13 @@ luaReturnCount_e post(lua_State *s) {
 class component final : public generic_component {
 public:
   void post_unpack() override {
-    static constexpr const luaL_Reg AxiosLibrary[3] = {
+    static constexpr const luaL_Reg AxiosLibrary[] = {
         {"Get", lua_state::unsafe_function<get>},
+        {"GetRetrieve", lua_state::unsafe_function<get>},
+        {"GetUpdate", lua_state::unsafe_function<get>},
+        {"GetUpdateEncrypted", lua_state::unsafe_function<get>},
         {"Post", lua_state::unsafe_function<post>},
+        {"PostEncrypted", lua_state::unsafe_function<post>},
         {nullptr, nullptr},
     };
     lua_state::register_lib("Axios", AxiosLibrary);
