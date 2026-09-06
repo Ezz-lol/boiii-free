f8bfaa5a6fa4e4556ea486dced45265bc7bd2ee2
Adam Killam <adammkillam@gmail.com>
2026-09-05 21:48:51 -0230

    Fix CL compilation

--- src/client/resource.hpp
+++ src/client/resource.hpp
@@ -3,13 +3,11 @@
 #define ID_ICON 102
 #define IMAGE_SPLASH 300
 
-#define IMAGE_LOGO 301
+#define DW_MOTD 301
+#define DW_CARDS 302
+#define DW_PLAYLISTS 303
+#define DW_FASTFILE 304
+#define DW_KEYS 305
+#define DW_QOSCONFIG 306
 
-#define DW_MOTD 302
-#define DW_CARDS 303
-#define DW_PLAYLISTS 304
-#define DW_FASTFILE 305
-#define DW_KEYS 306
-#define DW_QOSCONFIG 307
-
-#define TLS_DLL 308
+#define TLS_DLL 307
