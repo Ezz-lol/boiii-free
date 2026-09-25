a299a0bfa39c26909d2e70bcb2ee737b55e11fc4
Adam Killam <adammkillam@gmail.com>
2026-09-25 14:32:42 -0230

    Fix Con_DrawInput new client offset

--- src/client/game/symbols/console.hpp
+++ src/client/game/symbols/console.hpp
@@ -8,6 +8,6 @@ namespace con {
 WEAK symbol<void(char *text, int maxSize)> Con_GetTextCopy{
     0x14133A7F0, 0x14133A7D0, 0x140182C40};
 WEAK symbol<void(LocalClientNum_t localClientNum)> Con_DrawInput{
-    0x141339902, 0x1413398E0, 140182010};
+    0x141339900, 0x1413398E0, 140182010};
 } // namespace con
 } // namespace game
