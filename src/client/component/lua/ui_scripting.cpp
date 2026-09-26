8193088d78ac0626b52b6652f33d37c0a2b032ad
Adam Killam <adammkillam@gmail.com>
2026-09-25 21:48:23 -0230

    Fix dedicated server load

--- src/client/component/lua/ui_scripting.cpp
+++ src/client/component/lua/ui_scripting.cpp
@@ -2460,24 +2460,23 @@ public:
     hks_package_require_hook.create(
         game::select(0x141D1C6C0, 0x141D28EF0, 0x1403D7FC0),
         hks_package_require_stub);
+    ui_cod_init_hook.create(UI_CoD_Init, ui_cod_init_stub);
+    ui_cod_lobbyui_init_hook.create(UI_CoD_LobbyUI_Init,
+                                    ui_cod_lobbyui_init_stub);
+    // TODO: these inline offsets should be a symbol - strongly typed, named,
+    // and properly namespaced.
+    ui_shutdown_hook.create(game::select(0x142694C90, 0x14270DE00, 0x1404A1280),
+                            ui_shutdown_stub);
+    lua_cod_getrawfile_hook.create(Lua_CoD_GetRawFile.get(),
+                                   lua_cod_getrawfile_stub);
+
+    // TODO: these inline offsets should be a symbol - strongly typed, named,
+    // and properly namespaced.
+    hksi_lua_getinfo_detour.create(
+        game::select(0x141D40E40, 0x141D4D8D0, 0x1403F64B0),
+        hksi_lua_getinfo_stub);
+
     if (game::is_client()) {
-      ui_cod_init_hook.create(UI_CoD_Init, ui_cod_init_stub);
-      ui_cod_lobbyui_init_hook.create(UI_CoD_LobbyUI_Init,
-                                      ui_cod_lobbyui_init_stub);
-      // TODO: these inline offsets should be a symbol - strongly typed, named,
-      // and properly namespaced.
-      ui_shutdown_hook.create(
-          game::select(0x142694C90, 0x14270DE00, 0x1404A1280),
-          ui_shutdown_stub);
-      lua_cod_getrawfile_hook.create(Lua_CoD_GetRawFile.get(),
-                                     lua_cod_getrawfile_stub);
-
-      // TODO: these inline offsets should be a symbol - strongly typed, named,
-      // and properly namespaced.
-      hksi_lua_getinfo_detour.create(
-          game::select(0x141D40E40, 0x141D4D8D0, 0x1403F64B0),
-          hksi_lua_getinfo_stub);
-
       ui_init_hook.create(UI_Init.get(), ui_init_stub);
       cl_first_snapshot_hook.create(game::cl::CL_FirstSnapshot.get(),
                                     cl_first_snapshot_stub);
